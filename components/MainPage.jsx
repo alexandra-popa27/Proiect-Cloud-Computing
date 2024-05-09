2e994ed08dd7dcee584ac45f6345b16011f812fb
alexandra-popa27 <alexandradianapopa@yahoo.com>
2024-05-09 14:57:09 +0300

    added cards

--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -1,10 +1,13 @@
 import React,{useEffect,useState} from "react";
 import { deleteRecord, getRecords } from "@/utils/recordsFunctions";
 import Spinner from "./Spinner";
+import { useRouter } from "next/router";
+
 
 const MainPage=()=>{
     const [data,setData]=useState([]);
     const [isLoading,setIsLoading]=useState(true);
+    const router=useRouter();
 
     const fetchRecords = async () => {
         try {
@@ -18,6 +21,25 @@ const MainPage=()=>{
         }
     };
 
+    const handleDeleteRecord = async (id) => {
+        try {
+          const response = await deleteRecord(id);
+    
+          if (response.deletedCount === 1) {
+            const newData = data.filter((record) => record._id !== id);
+            setData(newData);
+          }
+        } catch (error) {
+          console.log(error)
+        }
+    };
+
+    const handleEditRecord=(id) => {
+        router.push(`/edit?id=${id}`)
+    }
+
+    
+
     useEffect(()=>{
         fetchRecords();
     },[]);
@@ -27,21 +49,42 @@ const MainPage=()=>{
     }
 
     return(
-        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="#">
-                <img className="rounded-t-lg" src="https://cdn.dummyjson.com/recipe-images/1.webp" alt="" />
-            </a>
-            <div className="p-5">
-                <a href="#">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                </a>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Read more
-                    <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                    </svg>
-                </a>
+        <div className="relative bg-beige p-4">
+            {/* Image with text overlay */}
+            <div className="relative h-96 overflow-hidden p-4">
+            <img className="absolute inset-0 w-full h-full object-cover" src="/cooking.jpg" alt="Cooking Hub Background" />
+            <div className="absolute inset-0 flex items-center justify-center text-white text-4xl font-bold tracking-tight text-center">
+                The Cooking Hub
+            </div>
+            </div>
+
+            {/*Cards*/}
+            <div className="p-4 flex flex-wrap justify-center gap-4">
+                {data?.map(record => (
+                    <div key={record._id} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <a href="#">
+                            <img className="rounded-t-lg" src={record.image} alt="" />
+                        </a>
+                        <div className="p-5">
+                            <a href="#">
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{record.name}</h5>
+                            </a>
+                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                                {`${record.cuisine} cuisine`}
+                                <br />
+                                Difficulty: {record.difficulty}
+                            </p>
+                            <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mr-4">
+                                Read more
+                                <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                </svg>
+                            </a>
+                            <button type="button" className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900 mr-4" onClick={()=>handleEditRecord(record._id)}>Edit</button>
+                            <button type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" onClick={() => handleDeleteRecord(record._id)}>Delete</button>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     )
